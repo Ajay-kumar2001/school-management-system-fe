e4011bc73baa7ae12703f95f1f6b28b1233a0fca
Kalyan Kumar <kalyankumar.balne@ahex.co.in>
2024-10-29 18:23:05 +0530

    Admin Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-import UserRegistrationForm  from '../src/common/UserRegistrationForm';
+// import UserRegistrationForm  from '../src/common/UserRegistrationForm';
+// import AdminPage from './components/admin/AdminPage';
+import AdminLayout from './components/layout/adminLayout';
 function App() {
   return (
     <div className="App">
-     <UserRegistrationForm/>
+      {/* <UserRegistrationForm/> */}
+      <AdminLayout  />
     </div>
   );
 }
