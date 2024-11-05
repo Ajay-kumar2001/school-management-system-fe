c2999eb2692299a81de17ec2ceb64aaf718d0318
Kalyan Kumar <kalyankumar.balne@ahex.co.in>
2024-11-05 14:41:08 +0530

    sidebar layout redesigned

--- src/common/sideBarMenu.tsx
+++ src/common/sideBarMenu.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const sideBarMenu = (props:any) => {
-    console.log(props.headline)
-    const Icon = props.icons
+const SideBarMenu = ({ headline, Icon, isActive, setActive }:any) => {
+  const isClickable = Icon !== null; // Determines if the item should be clickable
+
   return (
-    <div className='flex flex-row items-center '> <div className='pr-3'>{Icon?<Icon/>:<></> }</div>{props.headline}</div>
-  )
-}
+    <div
+      className={`flex flex-row items-center py-1 ${isClickable && isActive ? 'textColor font-semibold' : ''}`}
+      onClick={isClickable ? () => setActive(headline) : undefined} // Only set active if clickable
+    >
+      {isClickable ? (
+        <Link to={headline} className="flex flex-row items-center">
+          <div className="pr-3">{Icon && <Icon />}</div>
+          {headline}
+        </Link>
+      ) : (
+        // Render as plain text if not clickable
+        <div className="flex flex-row items-center text-gray-500">
+          {headline}
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default sideBarMenu
+export default SideBarMenu;
